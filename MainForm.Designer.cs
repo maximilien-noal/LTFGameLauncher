a7da276cc7223da52c70f28c1087ee72a685927d
Maximilien Noal <noal.maximilien@gmail.com>
2018-11-10 19:17:56 +0100

    First release

---
+++ MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace LTFGameLauncher
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.DisableGraphicalWrapperCheckBox = new System.Windows.Forms.CheckBox();
+            this.GraphicsWrapperCheckBoxLabel = new System.Windows.Forms.Label();
+            this.ManualButton = new System.Windows.Forms.Button();
+            this.SetupButton = new System.Windows.Forms.Button();
+            this.Action1Button = new System.Windows.Forms.Button();
+            this.Action2Button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PlayButton
+            // 
+            resources.ApplyResources(this.PlayButton, "PlayButton");
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // DisableGraphicalWrapperCheckBox
+            // 
+            resources.ApplyResources(this.DisableGraphicalWrapperCheckBox, "DisableGraphicalWrapperCheckBox");
+            this.DisableGraphicalWrapperCheckBox.Name = "DisableGraphicalWrapperCheckBox";
+            this.DisableGraphicalWrapperCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // dgVoodooLabel
+            // 
+            resources.ApplyResources(this.GraphicsWrapperCheckBoxLabel, "dgVoodooLabel");
+            this.GraphicsWrapperCheckBoxLabel.Name = "dgVoodooLabel";
+            // 
+            // ManualButton
+            // 
+            resources.ApplyResources(this.ManualButton, "ManualButton");
+            this.ManualButton.Name = "ManualButton";
+            this.ManualButton.UseVisualStyleBackColor = true;
+            this.ManualButton.Click += new System.EventHandler(this.ManualButton_Click);
+            // 
+            // SetupButton
+            // 
+            resources.ApplyResources(this.SetupButton, "SetupButton");
+            this.SetupButton.Name = "SetupButton";
+            this.SetupButton.UseVisualStyleBackColor = true;
+            this.SetupButton.Click += new System.EventHandler(this.SetupButton_Click);
+            // 
+            // Action1Button
+            // 
+            resources.ApplyResources(this.Action1Button, "Action1Button");
+            this.Action1Button.Name = "Action1Button";
+            this.Action1Button.UseVisualStyleBackColor = true;
+            this.Action1Button.Click += new System.EventHandler(this.Action1Button_Click);
+            // 
+            // Action2Button
+            // 
+            resources.ApplyResources(this.Action2Button, "Action2Button");
+            this.Action2Button.Name = "Action2Button";
+            this.Action2Button.UseVisualStyleBackColor = true;
+            this.Action2Button.Click += new System.EventHandler(this.Action2Button_Click);
+            // 
+            // MainForm
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Action2Button);
+            this.Controls.Add(this.Action1Button);
+            this.Controls.Add(this.SetupButton);
+            this.Controls.Add(this.ManualButton);
+            this.Controls.Add(this.GraphicsWrapperCheckBoxLabel);
+            this.Controls.Add(this.DisableGraphicalWrapperCheckBox);
+            this.Controls.Add(this.PlayButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.CheckBox DisableGraphicalWrapperCheckBox;
+        private System.Windows.Forms.Label GraphicsWrapperCheckBoxLabel;
+        private System.Windows.Forms.Button ManualButton;
+        private System.Windows.Forms.Button SetupButton;
+        private System.Windows.Forms.Button Action1Button;
+        private System.Windows.Forms.Button Action2Button;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Reflection;
+using System.IO;
+
+namespace LTFGameLauncher
+{
+    public partial class MainForm : Form
+    {
+        private string _workDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Text += string.Format(" {0}", Properties.Settings.Default.GameName);
+
+            if(string.IsNullOrWhiteSpace(Properties.Settings.Default.GraphicalWrapperName))
+            {
+                this.DisableGraphicalWrapperCheckBox.Visible = false;
+                this.GraphicsWrapperCheckBoxLabel.Visible = false;
+            }
+            this.DisableGraphicalWrapperCheckBox.Text = this.DisableGraphicalWrapperCheckBox.Text.Replace("dgVoodoo2", Properties.Settings.Default.GraphicalWrapperName);
+            this.DisableGraphicalWrapperCheckBox.Checked = Properties.Settings.Default.IsGraphicalWrapperDisabled;
+
+            Application.ApplicationExit += Application_ApplicationExit;
+
+            if (File.Exists(Path.Combine(_workDir, "manuel.pdf")))
+            {
+                ManualButton.Visible = true;
+            }
+            else if (File.Exists(Path.Combine(_workDir, "manual.pdf")))
+            {
+                ManualButton.Visible = true;
+            }
+            else
+            {
+                ManualButton.Visible = false;
+            }
+
+            if (File.Exists(Path.Combine(_workDir, "setup.exe")) == false)
+            {
+                SetupButton.Visible = false;
+            }
+
+            if(String.IsNullOrWhiteSpace(Properties.Settings.Default.AdditionAction1Exe))
+            {
+                Action1Button.Visible = false;
+            }
+            else
+            {
+                Action1Button.Text = Properties.Settings.Default.AdditionAction1Name;
+            }
+            if (String.IsNullOrWhiteSpace(Properties.Settings.Default.AdditionAction2Exe))
+            {
+                Action2Button.Visible = false;
+            }
+            else
+            {
+                Action2Button.Text = Properties.Settings.Default.AdditionAction2Name;
+            }
+
+            this.ManualButton.Location = new Point(Convert.ToInt32(this.Width / 2 - this.ManualButton.Width / 1.7), this.ManualButton.Location.Y);
+            this.SetupButton.Location = new Point(Convert.ToInt32(this.Width / 2 - this.SetupButton.Width / 1.7), this.SetupButton.Location.Y);
+            this.PlayButton.Location = new Point(Convert.ToInt32(this.Width / 2 - this.PlayButton.Width / 1.7), this.PlayButton.Location.Y);
+            this.DisableGraphicalWrapperCheckBox.Location = new Point(this.Width / 2 - this.DisableGraphicalWrapperCheckBox.Width / 2, this.DisableGraphicalWrapperCheckBox.Location.Y);
+            this.GraphicsWrapperCheckBoxLabel.Location = new Point(this.Width / 2 - this.GraphicsWrapperCheckBoxLabel.Width / 2, this.GraphicsWrapperCheckBoxLabel.Location.Y);
+        }
+
+        private void Application_ApplicationExit(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.IsGraphicalWrapperDisabled = this.DisableGraphicalWrapperCheckBox.Checked;
+            Properties.Settings.Default.Save();
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                EnableOrDisableGraphicsWrapper();
+                if(string.IsNullOrWhiteSpace(Properties.Settings.Default.WarningMessage) == false)
+                {
+                    MessageBox.Show(Properties.Settings.Default.WarningMessage, "Attention", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                Process.Start(Path.Combine(_workDir, "lancer.bat"));
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Pas de lancer.bat ? :(");
+            }
+        }
+
+        private void EnableOrDisableGraphicsWrapper()
+        {
+            try
+            {
+                string[] dgVoodooFiles = { "D3DImm.DLL", "DDraw.DLL", "D3D8.dll", "Glide.dll", "Glide2x.dll", "Glide3x.dll" };
+                foreach (var path in dgVoodooFiles)
+                {
+                    string fullPath = Path.Combine(_workDir, path);
+                    string fullPathDisabled = Path.Combine(_workDir, string.Format("{0}{1}", Path.GetFileNameWithoutExtension(path), "Disabled.dll"));
+                    RenameFile(fullPath, fullPathDisabled);
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        private void RenameFile(string ddrawPath, string ddrawDisabledPath)
+        {
+            if (this.DisableGraphicalWrapperCheckBox.Checked)
+            {
+                if (File.Exists(ddrawPath))
+                {
+                    if (File.Exists(ddrawDisabledPath))
+                    {
+                        File.Delete(ddrawDisabledPath);
+                    }
+                    File.Move(ddrawPath, ddrawDisabledPath);
+                }
+            }
+            else
+            {
+                if (File.Exists(ddrawDisabledPath))
+                {
+                    if (File.Exists(ddrawPath) == false)
+                    {
+                        File.Move(ddrawDisabledPath, ddrawPath);
+                    }
+                }
+            }
+        }
+
+        private void ManualButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                EnableOrDisableGraphicsWrapper();
+                if(File.Exists(Path.Combine(_workDir, "manuel.pdf")))
+                {
+                    Process.Start(Path.Combine(_workDir, "manuel.pdf"));
+                }
+                else if(File.Exists(Path.Combine(_workDir, "manual.pdf")))
+                {
+                    Process.Start(Path.Combine(_workDir, "manual.pdf"));
+                }
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Pas de manuel.pdf / manual.pdf ? :(");
+            }
+        }
+
+        private void SetupButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                EnableOrDisableGraphicsWrapper();
+                Process.Start(Path.Combine(_workDir, "Setup.exe"));
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Pas de setup.exe ? :(");
+            }
+        }
+
+        private void Action1Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                EnableOrDisableGraphicsWrapper();
+                Process.Start(Path.Combine(_workDir, Properties.Settings.Default.AdditionAction1Exe));
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Pas de " + Properties.Settings.Default.AdditionAction1Exe +  " ? :(");
+            }
+        }
+
+        private void Action2Button_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                EnableOrDisableGraphicsWrapper();
+                Process.Start(Path.Combine(_workDir, Properties.Settings.Default.AdditionAction2Exe));
+                Application.Exit();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Pas de " + Properties.Settings.Default.AdditionAction2Exe + " ? :(");
+            }
+        }
+    }
+}
