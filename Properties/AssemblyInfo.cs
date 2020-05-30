29613ccc7da8ba0afd6d2d10ae4684a98b59d658
Maximilien Noal <noal.maximilien@gmail.com>
2020-05-30 10:53:05 +0200

    Background.png/jpg image used if found
    Signed-off-by: Maximilien Noal <noal.maximilien@gmail.com>

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Abandonware-France")]
 [assembly: AssemblyProduct("LTFGameLauncher")]
-[assembly: AssemblyCopyright("Copyright ©  2019 Abandonware-France")]
+[assembly: AssemblyCopyright("Copyright ©  2020 Abandonware-France")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -47,5 +47,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.3.0.0")]
-[assembly: AssemblyFileVersion("1.3.0.0")]
+[assembly: AssemblyVersion("1.4.0.0")]
+[assembly: AssemblyFileVersion("1.4.0.0")]
--- ViewModel/MainViewModel.cs
+++ ViewModel/MainViewModel.cs
@@ -7,141 +7,49 @@ using System.Globalization;
 using System.IO;
 using System.Reflection;
 using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
 using System.Windows.Threading;
 
 namespace LTFGameLauncher.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
-        private readonly string _workDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-        private Process _proc = new Process();
         private readonly DispatcherTimer _waitTimer = new DispatcherTimer();
 
-        private string _windowTitle = "Lanceur ";
-
-        public string WindowTitle
-        {
-            get => _windowTitle;
-            set => Set(nameof(WindowTitle), ref _windowTitle, value);
-        }
-
-        private bool _isDisableGraphicalWrapperCheckBoxVisible = true;
-
-        public bool IsDisableGraphicalWrapperCheckBoxVisible
-        {
-            get => _isDisableGraphicalWrapperCheckBoxVisible;
-            set => Set(nameof(IsDisableGraphicalWrapperCheckBoxVisible), ref _isDisableGraphicalWrapperCheckBoxVisible, value);
-        }
-
-        private bool _isGraphicalWrapperDisabled;
-
-        public bool IsGraphicalWrapperDisabled
-        {
-            get => _isGraphicalWrapperDisabled;
-            set => Set(nameof(IsGraphicalWrapperDisabled), ref _isGraphicalWrapperDisabled, value);
-        }
-
-        private string _graphicsWrapperName = "dgVoodoo2";
-
-        public string GraphicsWrapperName
-        {
-            get => _graphicsWrapperName;
-            set => Set(nameof(GraphicsWrapperName), ref _graphicsWrapperName, value);
-        }
-
-        private bool _isAction1ButtonVisible = true;
-
-        public bool IsAction1ButtonVisible
-        {
-            get => _isAction1ButtonVisible;
-            set => Set(nameof(IsAction1ButtonVisible), ref _isAction1ButtonVisible, value);
-        }
-
-        private bool _isAction2ButtonVisible = true;
-
-        public bool IsAction2ButtonVisible
-        {
-            get => _isAction2ButtonVisible;
-            set => Set(nameof(IsAction2ButtonVisible), ref _isAction2ButtonVisible, value);
-        }
-
-        private bool _isAction3ButtonVisible = true;
-
-        public bool IsAction3ButtonVisible
-        {
-            get => _isAction3ButtonVisible;
-            set => Set(nameof(IsAction3ButtonVisible), ref _isAction3ButtonVisible, value);
-        }
-
-        private bool _isAction4ButtonVisible = true;
-
-        public bool IsAction4ButtonVisible
-        {
-            get => _isAction4ButtonVisible;
-            set => Set(nameof(IsAction4ButtonVisible), ref _isAction4ButtonVisible, value);
-        }
-
-        private bool _isManualButtonVisible = true;
-
-        public bool IsManualButtonVisible
-        {
-            get => _isManualButtonVisible;
-            set => Set(nameof(IsManualButtonVisible), ref _isManualButtonVisible, value);
-        }
+        private readonly string _workDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
 
         private string _action1Label = "";
 
-        public string Action1Label
-        {
-            get => _action1Label;
-            set => Set(nameof(Action1Label), ref _action1Label, value);
-        }
-
         private string _action2Label = "";
-
-        public string Action2Label
-        {
-            get => _action2Label;
-            set => Set(nameof(Action2Label), ref _action2Label, value);
-        }
 
         private string _action3Label = "";
 
-        public string Action3Label
-        {
-            get => _action3Label;
-            set => Set(nameof(Action3Label), ref _action3Label, value);
-        }
-
         private string _action4Label = "";
 
-        public string Action4Label
-        {
-            get => _action4Label;
-            set => Set(nameof(Action4Label), ref _action4Label, value);
-        }
+        private Uri _background;
+
+        private string _graphicsWrapperName = "dgVoodoo2";
+
+        private bool _isAction1ButtonVisible = true;
+
+        private bool _isAction2ButtonVisible = true;
+
+        private bool _isAction3ButtonVisible = true;
+
+        private bool _isAction4ButtonVisible = true;
+
+        private bool _isDisableGraphicalWrapperCheckBoxVisible = true;
+
+        private bool _isGraphicalWrapperDisabled;
+
+        private bool _isManualButtonVisible = true;
 
         private string _playButtonText = "";
 
-        public string PlayButtonText
-        {
-            get => _playButtonText;
-            set => Set(nameof(PlayButtonText), ref _playButtonText, value);
-        }
+        private Process _proc = new Process();
 
-        public bool IsPlayButtonTextUndefined => string.IsNullOrWhiteSpace(_playButtonText);
-
-        public RelayCommand PlayButtonCommand { get; private set; }
-
-        public RelayCommand Action1ButtonCommand { get; private set; }
-
-        public RelayCommand Action2ButtonCommand { get; private set; }
-
-        public RelayCommand Action3ButtonCommand { get; private set; }
-
-        public RelayCommand Action4ButtonCommand { get; private set; }
-
-        public RelayCommand ManualButtonCommand { get; private set; }
+        private string _windowTitle = "Lanceur ";
 
         public MainViewModel()
         {
@@ -151,6 +59,15 @@ namespace LTFGameLauncher.ViewModel
             Action2ButtonCommand = new RelayCommand(Action2ButtonCommand_Execute);
             Action3ButtonCommand = new RelayCommand(Action3ButtonCommand_Execute);
             Action4ButtonCommand = new RelayCommand(Action4ButtonCommand_Execute);
+
+            if (File.Exists("background.png"))
+            {
+                Background = new Uri(Path.Combine(_workDir, "background.png"));
+            }
+            else if (File.Exists("background.jpg"))
+            {
+                Background = new Uri(Path.Combine(_workDir, "background.jpg"));
+            }
 
             if (CultureInfo.CurrentUICulture.TwoLetterISOLanguageName.ToUpper() != "FR")
             {
@@ -224,56 +141,108 @@ namespace LTFGameLauncher.ViewModel
             }
         }
 
-        private void PlayButtonCommand_Execute()
+        public RelayCommand Action1ButtonCommand { get; private set; }
+
+        public string Action1Label
         {
-            try
-            {
-                EnableOrDisableGraphicsWrapper();
-                if (string.IsNullOrWhiteSpace(Properties.Settings.Default.WarningMessage) == false)
-                {
-                    System.Windows.MessageBox.Show(Properties.Settings.Default.WarningMessage, "Attention", MessageBoxButton.OK, MessageBoxImage.Information);
-                }
-                if (Properties.Settings.Default.KillProcessOnExit == false)
-                {
-                    Process.Start(Path.Combine(_workDir, Properties.Settings.Default.GameExecutable));
-                    App.Current.MainWindow.Close();
-                }
-                else
-                {
-                    _proc = Process.Start(Path.Combine(_workDir, Properties.Settings.Default.GameExecutable));
-                    App.Current.MainWindow.WindowState = WindowState.Minimized;
-                    _proc.WaitForInputIdle(5000);
-                    _waitTimer.Tick += WaitTimer_Tick;
-                    _waitTimer.Interval = TimeSpan.FromMilliseconds(2000);
-                    _waitTimer.IsEnabled = true;
-                    _waitTimer.Start();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Pas de " + Properties.Settings.Default.GameExecutable + " :(");
-            }
+            get => _action1Label;
+            set => Set(nameof(Action1Label), ref _action1Label, value);
         }
 
-        private void ManualButtonCommand_Execute()
+        public RelayCommand Action2ButtonCommand { get; private set; }
+
+        public string Action2Label
         {
-            try
-            {
-                EnableOrDisableGraphicsWrapper();
-                if (File.Exists(Path.Combine(_workDir, "manuel.pdf")))
-                {
-                    Process.Start(Path.Combine(_workDir, "manuel.pdf"));
-                }
-                else if (File.Exists(Path.Combine(_workDir, "manual.pdf")))
-                {
-                    Process.Start(Path.Combine(_workDir, "manual.pdf"));
-                }
-                App.Current.MainWindow.Close();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message, "Pas de manuel.pdf / manual.pdf ? :(");
-            }
+            get => _action2Label;
+            set => Set(nameof(Action2Label), ref _action2Label, value);
+        }
+
+        public RelayCommand Action3ButtonCommand { get; private set; }
+
+        public string Action3Label
+        {
+            get => _action3Label;
+            set => Set(nameof(Action3Label), ref _action3Label, value);
+        }
+
+        public RelayCommand Action4ButtonCommand { get; private set; }
+
+        public string Action4Label
+        {
+            get => _action4Label;
+            set => Set(nameof(Action4Label), ref _action4Label, value);
+        }
+
+        public Uri Background
+        {
+            get => _background;
+            set => Set(nameof(Background), ref _background, value);
+        }
+
+        public string GraphicsWrapperName
+        {
+            get => _graphicsWrapperName;
+            set => Set(nameof(GraphicsWrapperName), ref _graphicsWrapperName, value);
+        }
+
+        public bool IsAction1ButtonVisible
+        {
+            get => _isAction1ButtonVisible;
+            set => Set(nameof(IsAction1ButtonVisible), ref _isAction1ButtonVisible, value);
+        }
+
+        public bool IsAction2ButtonVisible
+        {
+            get => _isAction2ButtonVisible;
+            set => Set(nameof(IsAction2ButtonVisible), ref _isAction2ButtonVisible, value);
+        }
+
+        public bool IsAction3ButtonVisible
+        {
+            get => _isAction3ButtonVisible;
+            set => Set(nameof(IsAction3ButtonVisible), ref _isAction3ButtonVisible, value);
+        }
+
+        public bool IsAction4ButtonVisible
+        {
+            get => _isAction4ButtonVisible;
+            set => Set(nameof(IsAction4ButtonVisible), ref _isAction4ButtonVisible, value);
+        }
+
+        public bool IsDisableGraphicalWrapperCheckBoxVisible
+        {
+            get => _isDisableGraphicalWrapperCheckBoxVisible;
+            set => Set(nameof(IsDisableGraphicalWrapperCheckBoxVisible), ref _isDisableGraphicalWrapperCheckBoxVisible, value);
+        }
+
+        public bool IsGraphicalWrapperDisabled
+        {
+            get => _isGraphicalWrapperDisabled;
+            set => Set(nameof(IsGraphicalWrapperDisabled), ref _isGraphicalWrapperDisabled, value);
+        }
+
+        public bool IsManualButtonVisible
+        {
+            get => _isManualButtonVisible;
+            set => Set(nameof(IsManualButtonVisible), ref _isManualButtonVisible, value);
+        }
+
+        public bool IsPlayButtonTextUndefined => string.IsNullOrWhiteSpace(_playButtonText);
+
+        public RelayCommand ManualButtonCommand { get; private set; }
+
+        public RelayCommand PlayButtonCommand { get; private set; }
+
+        public string PlayButtonText
+        {
+            get => _playButtonText;
+            set => Set(nameof(PlayButtonText), ref _playButtonText, value);
+        }
+
+        public string WindowTitle
+        {
+            get => _windowTitle;
+            set => Set(nameof(WindowTitle), ref _windowTitle, value);
         }
 
         private void Action1ButtonCommand_Execute()
@@ -332,23 +301,6 @@ namespace LTFGameLauncher.ViewModel
             }
         }
 
-        private void WaitTimer_Tick(object sender, EventArgs e)
-        {
-            try
-            {
-                _proc.Refresh();
-                if (_proc.MainWindowHandle == IntPtr.Zero || _proc.MainWindowHandle == null)
-                {
-                    _proc.Kill();
-                    App.Current.MainWindow.Close();
-                }
-            }
-            catch
-            {
-                App.Current.MainWindow.Close();
-            }
-        }
-
         private void EnableOrDisableGraphicsWrapper()
         {
             try
@@ -363,6 +315,64 @@ namespace LTFGameLauncher.ViewModel
             }
             catch
             {
+            }
+        }
+
+        private void ManualButtonCommand_Execute()
+        {
+            try
+            {
+                EnableOrDisableGraphicsWrapper();
+                if (File.Exists(Path.Combine(_workDir, "manuel.pdf")))
+                {
+                    Process.Start(Path.Combine(_workDir, "manuel.pdf"));
+                }
+                else if (File.Exists(Path.Combine(_workDir, "manual.pdf")))
+                {
+                    Process.Start(Path.Combine(_workDir, "manual.pdf"));
+                }
+                App.Current.MainWindow.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Pas de manuel.pdf / manual.pdf ? :(");
+            }
+        }
+
+        private void OnAppExit(object sender, ExitEventArgs e)
+        {
+            Properties.Settings.Default.IsGraphicalWrapperDisabled = IsGraphicalWrapperDisabled;
+            Properties.Settings.Default.Save();
+        }
+
+        private void PlayButtonCommand_Execute()
+        {
+            try
+            {
+                EnableOrDisableGraphicsWrapper();
+                if (string.IsNullOrWhiteSpace(Properties.Settings.Default.WarningMessage) == false)
+                {
+                    System.Windows.MessageBox.Show(Properties.Settings.Default.WarningMessage, "Attention", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+                if (Properties.Settings.Default.KillProcessOnExit == false)
+                {
+                    Process.Start(Path.Combine(_workDir, Properties.Settings.Default.GameExecutable));
+                    App.Current.MainWindow.Close();
+                }
+                else
+                {
+                    _proc = Process.Start(Path.Combine(_workDir, Properties.Settings.Default.GameExecutable));
+                    App.Current.MainWindow.WindowState = WindowState.Minimized;
+                    _proc.WaitForInputIdle(5000);
+                    _waitTimer.Tick += WaitTimer_Tick;
+                    _waitTimer.Interval = TimeSpan.FromMilliseconds(2000);
+                    _waitTimer.IsEnabled = true;
+                    _waitTimer.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Pas de " + Properties.Settings.Default.GameExecutable + " :(");
             }
         }
 
@@ -391,10 +401,21 @@ namespace LTFGameLauncher.ViewModel
             }
         }
 
-        private void OnAppExit(object sender, ExitEventArgs e)
+        private void WaitTimer_Tick(object sender, EventArgs e)
         {
-            Properties.Settings.Default.IsGraphicalWrapperDisabled = IsGraphicalWrapperDisabled;
-            Properties.Settings.Default.Save();
+            try
+            {
+                _proc.Refresh();
+                if (_proc.MainWindowHandle == IntPtr.Zero || _proc.MainWindowHandle == null)
+                {
+                    _proc.Kill();
+                    App.Current.MainWindow.Close();
+                }
+            }
+            catch
+            {
+                App.Current.MainWindow.Close();
+            }
         }
     }
 }
